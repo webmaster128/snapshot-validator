74ea927e4a745a7e8aeef601272b4956b3a803b8
Simon Warta <simon@kullo.net>
2018-05-18 17:25:22 +0200

    Improve error message

--- state.cpp
+++ state.cpp
@@ -6,7 +6,9 @@ void validateState(const State state)
 {
     for (auto &addressBalance : state.balances) {
         if (addressBalance.second < 0 && addressBalance.first != 9594788837974552645ul) {
-            throw std::runtime_error("Negative balance for address " + std::to_string(addressBalance.first));
+            throw std::runtime_error(
+                        "Negative balance for address " + std::to_string(addressBalance.first) +
+                        ": " + std::to_string(addressBalance.second));
         }
     }
 }
