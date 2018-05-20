7940d42e276ef630e0ce7f74866135b282139f35
Simon Warta <simon@kullo.net>
2018-05-21 00:57:17 +0200

    Validate all transactions after block height 1

--- main.cpp
+++ main.cpp
@@ -258,10 +258,10 @@ int run(std::vector<std::string> args)
 
                     // Validate transaction
 
-                    if (t.type == 0 || (t.type == 2 && t.timestamp != 0)) {
-                        TransactionValidator::validate(transactionRow);
-                    } else {
+                    if (dbHeight == 1) {
                         std::cout << "Transaction not verified: " << t << std::endl;
+                    } else {
+                        TransactionValidator::validate(transactionRow);
                     }
 
                     // Update state from transaction
