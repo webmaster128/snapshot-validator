ef7a09957b7784f2ca98b31abe17d3ac73d4abdc
Simon Warta <simon@kullo.net>
2018-05-21 10:15:26 +0200

    Verify type 0 in genesis

--- main.cpp
+++ main.cpp
@@ -258,7 +258,7 @@ int run(std::vector<std::string> args)
 
                     // Validate transaction
 
-                    if (dbHeight == 1) {
+                    if (dbHeight == 1 && t.type != 0) {
                         std::cout << "Transaction not verified: " << t << std::endl;
                     } else {
                         std::vector<unsigned char> secondSignatureRequiredBy;
