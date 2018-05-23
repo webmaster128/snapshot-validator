0d4fcc3caf89d825ce2d1604187bd38f88619a2e
Simon Warta <simon@kullo.net>
2018-05-23 15:25:16 +0200

    Add height comments to exceptions

--- settings.cpp
+++ settings.cpp
@@ -7,9 +7,9 @@ Settings::Settings(Network network)
         negativeBalanceAddress = 16009998050678037905ul;
         v100Compatible = false;
         exceptions.invalidTransactionSignature = {
-            3274071402587084244ul,
-            10403141873189588012ul,
-            16896494584440078079ul,
+            3274071402587084244ul,  // 595491
+            10403141873189588012ul, // 624550
+            16896494584440078079ul, // 631670
         };
         exceptions.balanceAdjustments[15449731671927352923ul] = -1*100000000; // Burned 1 LSK as `amount` in a delegate vote
         break;
