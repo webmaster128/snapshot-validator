6e859e68c07fb1b72ad441c881b59a34d99bec33
Simon Warta <simon@kullo.net>
2018-05-27 15:21:45 +0200

    Update types in BlockchainState

--- blockchain_state.h
+++ blockchain_state.h
@@ -1,15 +1,17 @@
 #pragma once
 
+#include <string>
 #include <unordered_map>
 #include <vector>
 
 #include "settings.h"
 #include "transaction.h"
+#include "types.h"
 
 struct BlockchainState {
-    std::unordered_map<std::uint64_t, std::int64_t> balances;
-    std::unordered_map<std::uint64_t, std::vector<unsigned char>> secondPubkeys;
-    std::unordered_map<std::uint64_t, std::string> delegateNames;
+    std::unordered_map<address_t, std::int64_t> balances;
+    std::unordered_map<address_t, bytes_t> secondPubkeys;
+    std::unordered_map<address_t, std::string> delegateNames;
 
     void applyTransaction(const Transaction &transaction);
     void validate(const Settings &settings) const;
