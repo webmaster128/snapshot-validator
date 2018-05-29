dbb9715240aca0b17134c6705e8247f64fc03768
Simon Warta <simon@kullo.net>
2018-05-29 10:38:50 +0200

    Extract validateId, validateSignature

--- block_header_validator.cpp
+++ block_header_validator.cpp
@@ -5,21 +5,37 @@
 
 #include "utils.h"
 
-namespace BlockHeaderValidator {
+namespace {
 
-void validate(const BlockHeader &bh, std::uint64_t dbId, const bytes_t &signature) {
+void validateId(const BlockHeader &bh, std::uint64_t dbId, const bytes_t &signature)
+{
     auto id = bh.id(signature);
-    if (id != dbId) {
-        throw std::runtime_error("id mismatch");
+    if (id != dbId)
+    {
+        throw std::runtime_error("Block ID mismatch");
     }
+}
 
+void validateSignature(const BlockHeader &bh, std::uint64_t dbId, const bytes_t &signature)
+{
     auto hash = bh.hash();
     if (crypto_sign_verify_detached(signature.data(), hash.data(), hash.size(), bh.generatorPublicKey.data()) != 0) {
-        std::cout << "ID: " << dbId << std::endl;
+        std::cout << "Height: " << dbId << std::endl;
         std::cout << "Pubkey: " << bytes2Hex(bh.generatorPublicKey) << std::endl;
         std::cout << "Signature: " << bytes2Hex(signature) << std::endl;
         throw std::runtime_error("Invalid block signature");
     }
+}
+
+
+}
+
+namespace BlockHeaderValidator {
+
+void validate(const BlockHeader &bh, std::uint64_t dbId, const bytes_t &signature)
+{
+    validateId(bh, dbId, signature);
+    validateSignature(bh, dbId, signature);
 }
 
 }
